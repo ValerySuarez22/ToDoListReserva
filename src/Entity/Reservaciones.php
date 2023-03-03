0facba86176017a1734cd5c6fba1a3f0a4550944
ValerySuarez22 <valerysuarez2296@gmail.com>
2023-03-03 15:15:07 +0100

    cambios con css

---
+++ src/Entity/Reservaciones.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservacionesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservacionesRepository::class)]
+class Reservaciones
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $usuario = null;
+
+    #[ORM\Column]
+    private ?int $telefono = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fecha = null;
+
+    #[ORM\ManyToOne(inversedBy: 'reservaciones')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Categorias $Habitacion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsuario(): ?string
+    {
+        return $this->usuario;
+    }
+
+    public function setUsuario(string $usuario): self
+    {
+        $this->usuario = $usuario;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?int
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(int $telefono): self
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): self
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function getHabitacion(): ?Categorias
+    {
+        return $this->Habitacion;
+    }
+
+    public function setHabitacion(?Categorias $Habitacion): self
+    {
+        $this->Habitacion = $Habitacion;
+
+        return $this;
+    }
+}
